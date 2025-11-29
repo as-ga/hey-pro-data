30d7ebaa5725506b2ab7775e7a524a5b80477adc
Anant Kumar <anantkumar012002@gmail.com>
2025-11-29 18:35:18 +0530

    Refactor VisaSection component layout: remove fixed height and adjust width for improved responsiveness

--- app/(app)/profile/components/visa.tsx
+++ app/(app)/profile/components/visa.tsx
@@ -182,9 +182,9 @@ export default function VisaSection({
                     VISA
                 </div>
             </DialogTrigger>
-            <DialogContent className="w-full max-w-[430px] h-[500px]  overflow-x-auto border-none bg-transparent p-0">
+            <DialogContent className="w-full   overflow-x-auto border-none bg-transparent no-scrollbar p-0">
                 <form onSubmit={handleSubmit}>
-                    <div className="mx-auto flex h-full max-h-[613px] w-full max-w-[393px] flex-col rounded-t-[20px] bg-white px-5 pb-6 pt-[25px] shadow-[0_8px_18px_rgba(0,0,0,0.1)]">
+                    <div className="mx-auto flex h-full w-full flex-col bg-white px-5 pb-6 pt-[25px] shadow-[0_8px_18px_rgba(0,0,0,0.1)]">
                         <div className="mx-auto mb-6 h-[5px] w-[150px] rounded-full bg-[#868686]" />
                         <div className="flex flex-col gap-[25px]">
                             <div className="space-y-2">
